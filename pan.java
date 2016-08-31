5b64ea4f673e149e5b5aca919f8b8a73d02b44cd
Nandy21 <nandhinimuthulakshmi@gmail.com>
2016-08-31 23:48:01 +0530

    Create pan.java

---
+++ pan.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+
+public class pangram {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		char c=' ';
+		String str=s.toUpperCase();
+		int count,ana=0;
+		
+		for(int i=0;i<26;i++)
+		{
+			System.out.println(i);
+			count=0;
+			c='A';
+			for(int j=0;j<str.length();j++)
+			{
+				if(c==str.charAt(j))
+				{
+				count++;
+				ana++;
+				System.out.println("count"+count+"ana"+ana);
+				}
+			
+			}
+			if(count!=1)
+			{
+				System.out.println("String is not a pangram");
+				break;
+			}
+			c++;
+		}
+		if(ana==26)
+		{
+			System.out.println("String is a Pangram");
+			System.out.println("res"+ana);
+		}
+
+	}
+
+}
